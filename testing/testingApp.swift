cf9d6338102b5a69a2839212fa9396f7ff9a8d82
justBearded <vanamburg.mv@gmail.com>
2022-06-19 18:20:00 -0500

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Mike  Van Amburg on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
